ad81c7a0b939b91d27903100f6affcd2306992ed
簫羽 <zi_mengling@yeah.net>
2016-03-31 14:58:17 +0800

    add index route

---
+++ hello.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# 学习flask框架
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return '<h1>Hello, world!</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+

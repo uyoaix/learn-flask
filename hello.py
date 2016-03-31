fa4ca44c3b94d5394791e7f9371e7163a05b8a34
簫羽 <zi_mengling@yeah.net>
2016-03-31 16:02:35 +0800

    use Jinja2 ext renders template

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@
 # 学习flask框架
 
 
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.script import Manager
 
 app = Flask(__name__)
@@ -12,11 +12,11 @@ manager = Manager(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello, world!</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!' % name
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
     manager.run()

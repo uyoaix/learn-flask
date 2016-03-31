ed694859fff58b419d5a5dac69290f69277252cd
簫羽 <zi_mengling@yeah.net>
2016-03-31 15:07:18 +0800

    add dynamic parm route

--- hello.py
+++ hello.py
@@ -12,6 +12,10 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello, world!</h1>'
 
+@app.route('user/<name>')
+def user(name):
+    return '<h1>Hello, %s!' % name
+
 if __name__ == '__main__':
     app.run(debug=True)
 

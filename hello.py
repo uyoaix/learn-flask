d0d57a4a0b6e0e4e3e3cb911155ee697dff321ec
簫羽 <zi_mengling@yeah.net>
2016-04-22 16:17:58 +0800

    handle form with flask-wtf

--- hello.py
+++ hello.py
@@ -9,15 +9,28 @@ from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'You can do what?'
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+	name = form.name.data
+	form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):

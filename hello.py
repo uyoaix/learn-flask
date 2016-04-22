006765356965de77e9872665955e817405608d2e
簫羽 <zi_mengling@yeah.net>
2016-04-22 16:31:23 +0800

    post/redirect/get

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@
 # 学习flask框架
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
@@ -25,12 +25,11 @@ class NameForm(Form):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-	name = form.name.data
-	form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+	return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):

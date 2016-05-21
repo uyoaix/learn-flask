b10e21b51b1cde97787319157296c4cdbce35ac7
簫羽 <zi_mengling@yeah.net>
2016-05-21 22:40:30 +0800

    fix Model object repr method

--- hello.py
+++ hello.py
@@ -58,9 +58,9 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role')
+    users = db.relationship('User', backref='role', lazy='dynamic')
 
-    def _repr_(self):
+    def __repr__(self):
         return '<Role %r>' % self.name
 
 
@@ -70,7 +70,7 @@ class User(db.Model):
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
 
-    def _repr_(self):
+    def __repr__(self):
         return '<User %r>' % self.username
 
 

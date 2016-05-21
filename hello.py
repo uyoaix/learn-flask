a3d4b7dfca3ce9043bb3dcaa524a0e9a75a0bf61
簫羽 <zi_mengling@yeah.net>
2016-05-21 22:20:23 +0800

    fix Model define and make code match PEP8 syntax

--- hello.py
+++ hello.py
@@ -8,7 +8,6 @@ from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
-from datetime import datetime
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
@@ -19,9 +18,9 @@ manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 app.config['SECRET_KEY'] = 'You can do what?'
-app.config['SQLALCHEMY_DATABASE_URI']='mysql://root:xingchen@localhost/test'
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:xingchen@localhost/test'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 
@@ -30,46 +29,50 @@ class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
         session['name'] = form.name.data
-	return redirect(url_for('index'))
+    return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'))
 
-@app.route('/user/<name>')
+
+@app.route("/user/<name>")
 def user(name):
     return render_template('user.html', name=name)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+
 class Role(db.Model):
-    _tablename_ = 'roles'
+    __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
     users = db.relationship('User', backref='role')
 
     def _repr_(self):
         return '<Role %r>' % self.name
-    
+
+
 class User(db.Model):
-    _tablename_ = 'users'
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    
-    def _repr_(self):
-        return '<User %r>' % self.username    
 
+    def _repr_(self):
+        return '<User %r>' % self.username
 
 
 if __name__ == '__main__':
     manager.run()
-
